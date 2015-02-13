9db41f27e170ccceb40007cd5d9d6038120ce3e3
hazeez <hafizul.azeez@gmail.com>
2015-02-13 13:15:35 +0530

    working copy with jquery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(".desktop-list").hide()
+$(".laptop-list").hide()
+$(document).ready(function(){
+    $(".computer").hover(function(){
+        $(".desktop-list").toggle(300);
+    });
+    $(".laptop").hover(function(){
+        $(".laptop-list").toggle(300);
+    });
+});
