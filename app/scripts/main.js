04e0cd30c392dc75379958f49b3feda330a5bed0
hazeez <hafizul.azeez@gmail.com>
2015-02-14 08:33:55 +0530

    index with accessories page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,36 @@
+
+// hide all the list items before page load
 $(".desktop-list").hide()
 $(".laptop-list").hide()
+$(".mobile-list").hide()
+$(".tablet-list").hide()
+$(".servers-list").hide()
+$(".cctv-list").hide()
+
+// when the page gets ready
 $(document).ready(function(){
-    $(".computer").hover(function(){
+// on hover show the list div items
+    $(".category-desktop").hover(function(){
         $(".desktop-list").toggle(300);
     });
-    $(".laptop").hover(function(){
+
+    $(".category-laptop").hover(function(){
         $(".laptop-list").toggle(300);
+    });
+
+    $(".category-mobiles").hover(function(){
+        $(".mobile-list").toggle(300);
+    });
+
+    $(".category-tablet").hover(function(){
+        $(".tablet-list").toggle(300);
+    });
+
+    $(".category-servers").hover(function(){
+        $(".servers-list").toggle(300);
+    });
+
+    $(".category-cctv").hover(function(){
+        $(".cctv-list").toggle(300);
     });
 });
