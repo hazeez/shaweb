7da3c139a9cc86b48364e4a753020c00613dc4bf
hazeez <hafizul.azeez@gmail.com>
2015-02-28 08:55:31 +0530

    almost complete webpage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 
+// $(".contact-bar").hide()
 // hide all the list items before page load
+
 $(".desktop-list").hide()
 $(".laptop-list").hide()
 $(".mobile-list").hide()
@@ -10,6 +12,10 @@ $(".cctv-list").hide()
 // when the page gets ready
 $(document).ready(function(){
 // on click show the list div items
+    // $(".contact-menu").click(function(){
+    //     $(".contact-bar").toggle();
+    // });
+
     $(".category-desktop").click(function(){
         $(".desktop-list").toggle(300);
     });
@@ -33,4 +39,20 @@ $(document).ready(function(){
     $(".category-cctv").click(function(){
         $(".cctv-list").toggle(300);
     });
+
+    // for page scorlling animation
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+    });
+
 });
